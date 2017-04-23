a89f60005c1d07302ab3b5727f719f1478887f94
EdOverflow <diopterasec@gmail.com>
2017-04-23 11:50:58 +0200

    Update timing_attack.py

--- demo/timing_attack.py
+++ demo/timing_attack.py
@@ -1,10 +1,48 @@
-import time
-import string
+import time, hmac
 
 
-characters = string.ascii_lowercase
-test_string = 'digest_'
-for c in characters:
-    if test_string != "digest_xyz":
-        print(time.time())
-        test_string += c
+def timing(f):
+    def wrap(*args):
+        time1 = time.time()
+        ret = f(*args)
+        time2 = time.time()
+        print '%s took %0.3f ms' % (f.func_name, (time2-time1)*1000.0)
+        return ret
+    return wrap
+
+@timing
+def timing_attack_diff_tokens():
+    token_1 = "100000000000000000000000000000000"
+    token_2 = "000000000000000000000000000000001"
+    for i in range(200):
+        if not token_1 == token_2:
+            print i
+
+@timing
+def timing_attack_same_tokens():
+    token_1 = "100000000000000000000000000000000"
+    token_2 = "100000000000000000000000000000000"
+    for i in range(200):
+        if token_1 == token_2:
+            print i
+
+@timing
+def constant_time_diff_tokens():
+    token_1 = b"100000000000000000000000000000000"
+    token_2 = b"000000000000000010000000000000000"
+    for i in range(200):
+        if not hmac.compare_digest(token_1, token_2):
+            print i
+        
+@timing
+def constant_time_same_tokens():
+    token_1 = b"100000000000000000000000000000000"
+    token_2 = b"100000000000000000000000000000000"
+    for i in range(200):
+        if hmac.compare_digest(token_1, token_2):
+            print i
+
+timing_attack_diff_tokens()
+timing_attack_same_tokens()
+constant_time_diff_tokens()
+constant_time_same_tokens()
